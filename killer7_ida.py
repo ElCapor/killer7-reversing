f5a397e4b619b9c9dada83a8e253631519efa8a6
ElCapor <104172250+ElCapor@users.noreply.github.com>
2024-06-05 22:47:42 +0200

    Update killer7_ida.py

--- killer7_ida.py
+++ killer7_ida.py
@@ -1,7 +1,20 @@
 import idaapi
 import idc
 import idautils
+import re
 assert_address = 0x42C400 #Address of the assert function
+
+def get_string_at_address(address):
+    string = idc.get_strlit_contents(address)
+    if type(string) == bytes:
+        return string.decode('utf-8')
+    return None
+
+# Function to sanitize function names by replacing special characters
+def sanitize_function_name(name):
+    # Replace characters that are not allowed in IDA names with underscores
+    sanitized_name = re.sub(r' ', '_', name)
+    return sanitized_name
 
 for xref in idautils.XrefsTo(assert_address, 0):
     print(xref.type, idautils.XrefTypeName(xref.type), 'from', hex(xref.frm), 'to', hex(xref.to))
@@ -21,6 +34,12 @@ for xref in idautils.XrefsTo(assert_address, 0):
             print(hex(insn[0].Op1.value))
             dism_addr -= insn[0].size
             args.append(insn[0].Op1.value)
+            string_at_arg = get_string_at_address(insn[0].Op1.value)
+            if string_at_arg != None:
+                print(f"Argument {i+1} (string): {string_at_arg} {sanitize_function_name(string_at_arg)}")
+    
+    idc.set_name(func.start_ea,"")
+                
         
     print([hex(arg) for arg in args])
     
