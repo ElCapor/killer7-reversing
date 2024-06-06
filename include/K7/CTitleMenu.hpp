2f29187ec5b1d1a5e0b1c74a919ace2d9e600284
ElCapor <104172250+ElCapor@users.noreply.github.com>
2024-06-06 23:04:51 +0200

    Reversed Colors

--- include/K7/CTitleMenu.hpp
+++ include/K7/CTitleMenu.hpp
@@ -5,10 +5,38 @@ namespace K7
 {
     namespace Graphics
     {
+        struct MenuButtonDescriptor
+        {
+            int text_len;
+            const char* text;
+            void* flag1;
+            void* flag2;
+            int flag4;
+            void* flag5;
+        };
+
+        struct Color
+        {
+            uint8_t r;
+            uint8_t g;
+            uint8_t b;
+            uint8_t a;
+        };
+
         struct CMenuTitle
         {
-            char pad_0000[7840]; //0x0000
+            char pad_0000[7736]; //0x0000
+            MenuButtonDescriptor PressStartButton;
+            MenuButtonDescriptor ContinueButton;
+            MenuButtonDescriptor OptionsButton;
+            MenuButtonDescriptor ExitButton;
+            char pad_1E98[8]; //0x1E98
 	        int32_t GameState; //0x1EA0
+            int32_t SelectedButtonIndex; //0x1EA4
+	        char pad_1EA8[31320]; //0x1EA8
+            Color color1;
+            Color color2;
+
             /*
             * STATES : 0x0 = PRESS START
             * STATES : 0x2 = MENU TITLE
--- src/K7/Misc.cpp
+++ src/K7/Misc.cpp
@@ -3,16 +3,18 @@
 
 K7::Graphics::DrawMenuButtonT K7::Graphics::oDrawMenuButton = (K7::Graphics::DrawMenuButtonT)ASLR(0x5A9380);
 
-uintptr_t CTitleMenuGUI = NULL;
+
 bool isAdded = false;
 bool __fastcall K7::Graphics::DrawMenuButton(uintptr_t _this, void* unused, MenuButtonDescriptor* a2, uint16_t x, uint16_t y, const char* text)
 {
     auto ret = oDrawMenuButton(_this, a2, x, y, text);
     if (!isAdded && strcmp(text, "NEW GAME") == 0)
     {
-        CTitleMenuGUI = _this;
-        oDrawMenuButton(_this, a2, 64, 270, "TruthRe");
-        isAdded = true;
+        auto ctitle = (CMenuTitle*)_this;
+        Console::log(ctitle->GameState);   
+        ctitle->color1.r = 0x00;
+        ctitle->color1.g = 0xFF;
+        ctitle->color1.b = 0x00;
     }
     Console::log(__FUNCTION__, " Called with arguments " , std::hex , _this, a2, std::dec, x, y, text);
     return ret;
